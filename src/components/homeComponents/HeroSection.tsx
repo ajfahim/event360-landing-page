1f9dd6ec5f40f6bd905f0bdda25c0d326b31b704
ajfahim <ajfahim52@gmail.com>
2024-01-29 15:25:29 +0600

    hero animation

--- src/components/homeComponents/HeroSection.tsx
+++ src/components/homeComponents/HeroSection.tsx
@@ -1,15 +1,46 @@
 import HeroImage from "@/assets/images/HeroImage.png";
+import { motion } from "framer-motion";
 
+const heroStaggerAnimationVariation = {
+  initial: { opacity: 0, y: -20 },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, staggerChildren: 0.3 },
+  },
+};
 const HeroSection = () => {
   return (
     <div className="w-full my-16 text-center">
-      <h2 className="text-6xl font-bold mb-2">Brand New event Packages</h2>
-      <h3 className="gradient-text font-bold text-5xl mb-3">For Winter</h3>
-      <p className="text-lg mb-10">
-        Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
-        ornare viverra. <br /> Ultrices faucibus neque velit risus ac id lorem.
-      </p>
-      <img src={HeroImage} alt="Hero Image" className="w-full" />
+      <motion.div
+        variants={heroStaggerAnimationVariation}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.h2 variants={heroStaggerAnimationVariation} className="mb-2">
+          Brand New event Packages
+        </motion.h2>
+        <motion.h3
+          variants={heroStaggerAnimationVariation}
+          className="gradient-text font-bold text-5xl mb-3"
+        >
+          For Winter
+        </motion.h3>
+        <motion.p
+          variants={heroStaggerAnimationVariation}
+          className="text-lg mb-10"
+        >
+          Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+          ornare viverra. <br /> Ultrices faucibus neque velit risus ac id
+          lorem.
+        </motion.p>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 500 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+      >
+        <img src={HeroImage} alt="Hero Image" className="w-full" />
+      </motion.div>
     </div>
   );
 };
